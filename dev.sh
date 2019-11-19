957457d2bde0dd613aa6e4fe4df1dfe4fefeafea
Den Pol <pravosleva@uremont.com>
2019-11-19 17:05:16 +0300

    master. Started

--- dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# DEPRECATED: Use pm2 instead of this
-
-fuser -k -n tcp 3110 &
-fuser -k -n tcp 3000 &
-DATABASE_HOST= DATABASE_PORT= DATABASE_NAME= DATABASE_USERNAME= DATABASE_PASSWORD= DATABASE_AUTHENTICATION_DATABASE= yarn --cwd ./backend develop &&
-NODE_ENV="development" yarn --cwd ./frontend start
